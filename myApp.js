0de94c51ea1abbd3b20287f609bccd202e18d24f
Daniel Loh <daniel@yeloh.net>
2024-04-10 15:03:45 +0000

    json

--- myApp.js
+++ myApp.js
@@ -1,13 +1,23 @@
 let express = require('express');
 let app = express();
 
+console.log("Hello World");
 
+/*
+app.get("/",function(req, res) {
+    res.send('Hello Express');
+  });
+*/
 
+app.get("/",function(req, res) {
+    res.sendFile(__dirname + '/views/index.html');
+  });
 
+app.use('/public',express.static(__dirname + '/public'));
 
-
-
-
+app.get("/json",function(req, res){
+  res.json({"message": "Hello json"});
+});
 
 
 
